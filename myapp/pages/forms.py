2e5d511904b5f2796678228ddd73c7ce05dc7c4f
Baibhab-7279 <mrbaibhab7279@gmail.com>
2022-07-21 20:30:14 +0530

    2nd_commi

--- myapp/pages/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class contactform(forms.Form):
-    yourname = forms.CharField(max_length=100,label="your name")
-    email = forms.EmailField(required=False, label="your email address")
-    subject = forms.CharField(max_length=100)
-    message = forms.CharField(widget=forms.Textarea)
